a155a60c816486d1d4cfb79a7f0cd2d13d82a3ee
Marvin Prochazka <git@kr0n05.de>
2018-12-15 16:08:31 +0100

    swamp optimize

--- kr0n05_de___21025/test/tasks.js
+++ kr0n05_de___21025/test/tasks.js
@@ -2,9 +2,9 @@ var tasks = {
 
     /** @param {Creep} creep **/
     plan: function() {
-        var pathToController = Game.spawns['Spawn1'].room.findPath(Game.spawns['Spawn1'].pos, Game.spawns['Spawn1'].room.controller.pos, {ignoreCreeps: true, swampCost: 1});
+        var pathToController = Game.spawns['Spawn1'].room.findPath(Game.spawns['Spawn1'].pos, Game.spawns['Spawn1'].room.controller.pos, {ignoreCreeps: true, swampCost: 2});
         for(var j = 0; j < Game.spawns['Spawn1'].room.find(FIND_SOURCES).length; j++){
-            var pathToNode1 = Game.spawns['Spawn1'].room.findPath(Game.spawns['Spawn1'].pos, Game.spawns['Spawn1'].room.find(FIND_SOURCES)[j].pos, {ignoreCreeps: true, swampCost: 1});
+            var pathToNode1 = Game.spawns['Spawn1'].room.findPath(Game.spawns['Spawn1'].pos, Game.spawns['Spawn1'].room.find(FIND_SOURCES)[j].pos, {ignoreCreeps: true, swampCost: 2});
             for(var i = 0; i<pathToNode1.length-1; i++){
                 Game.spawns['Spawn1'].room.createConstructionSite(
                     pathToNode1[i].x,
