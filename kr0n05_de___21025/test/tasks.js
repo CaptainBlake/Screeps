16e36c1ce283cf3a1a9b2ab57616c5a737c705ab
Captain Blake <Joshi_neuwirth@yahoo.de>
2018-12-15 15:59:50 +0100

    Minor Road-fix

--- kr0n05_de___21025/test/tasks.js
+++ kr0n05_de___21025/test/tasks.js
@@ -5,7 +5,7 @@ var tasks = {
         var pathToController = Game.spawns['Spawn1'].room.findPath(Game.spawns['Spawn1'].pos, Game.spawns['Spawn1'].room.controller.pos, {ignoreCreeps: true, swampCost: 1});
         for(var j = 0; j < Game.spawns['Spawn1'].room.find(FIND_SOURCES).length; j++){
             var pathToNode1 = Game.spawns['Spawn1'].room.findPath(Game.spawns['Spawn1'].pos, Game.spawns['Spawn1'].room.find(FIND_SOURCES)[j].pos, {ignoreCreeps: true, swampCost: 1});
-            for(var i = 0; i<pathToNode1.length; i++){
+            for(var i = 0; i<pathToNode1.length-1; i++){
                 Game.spawns['Spawn1'].room.createConstructionSite(
                     pathToNode1[i].x,
                     pathToNode1[i].y,
