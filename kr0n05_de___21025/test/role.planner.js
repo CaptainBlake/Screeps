5033865a7be4b0ea81367bb31b6dca52941877a9
Marvin Prochazka <git@kr0n05.de>
2018-12-15 02:07:23 +0100

    new planner class

---
+++ kr0n05_de___21025/test/role.planner.js
@@ -0,0 +1,17 @@
+
+var rolePlanner = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+            if(creep.memory.planToCon) {
+                creep.moveTo(creep.room.controller, {reusePath: 50, visualizePathStyle: {stroke: '#ffffff'}});
+                Game.spawns['Spawn1'].room.createConstructionSite(
+                    creep.pos.x,
+                    creep.pos.y,
+                    STRUCTURE_ROAD);
+            }
+        }
+    };
+
+module.exports = roleUpgrader;
