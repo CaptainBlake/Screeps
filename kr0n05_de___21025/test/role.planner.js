3922810cf9f55431158f8497810d64b5baf623a3
Marvin Prochazka <git@kr0n05.de>
2018-12-15 02:31:38 +0100

    Planner works

--- kr0n05_de___21025/test/role.planner.js
+++ kr0n05_de___21025/test/role.planner.js
@@ -4,8 +4,9 @@ var rolePlanner = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-            if(creep.memory.planToCon) {
+            if(creep.memory.planToCon == true) {
                 creep.moveTo(creep.room.controller, {reusePath: 50, visualizePathStyle: {stroke: '#ffffff'}});
+                creep.say('✏');
                 Game.spawns['Spawn1'].room.createConstructionSite(
                     creep.pos.x,
                     creep.pos.y,
